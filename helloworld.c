cccdccda3cb59c6530b1fb3812de033c32556505
Michael Yakovlev <yakovlev.michael@icloud.com>
2021-09-07 09:39:35 -0700

    created the project; basics in C

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x; //declaration
+	x = 1; //initialization
+	int y = 10; // both together
+
+	int z = x+y;
+	printf("hello world %d",z);
+
+	return 0;
+}
