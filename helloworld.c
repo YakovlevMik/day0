c39f42ef66a82c6f0f4c0024eb1a813951b56ee6
Michael Yakovlev <yakovlev.michael@icloud.com>
2021-09-07 10:57:36 -0700

    First Task: find mean & STD of a size-10 array

--- helloworld.c
+++ helloworld.c
@@ -4,14 +4,29 @@
 
 int main()
 {
-	int x; //declaration
-	x = 1; //initialization
-	int y = 10; // both together
 
-	int z = x+y;
-	printf("hello world %d + %d = %d",x,y,z);
+	double x[] = {11,10,1,66,99,45,27,39,81,5};
+
+	double y = 0;
+	double z = 0;
+
+	int i=0;
+	while (i<10){
+		y=y+x[i];
+		i++;
+	}
+	y=y/i;
+	printf("\nMean of the array = %f", y);
+
+	i=0;
+	while (i<10){
+		z=z+pow(y-x[i],2);
+		i++;
+	}
+	z=z/(i-1);
+	printf("\nVariance the array = %f", z);
+	z=pow(z,0.5);
+	printf("\nStandard Deviation of the array = %f", z);
 
 	return 0;
 }
-
-// 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
